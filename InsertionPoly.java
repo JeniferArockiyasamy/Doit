db49479cfdad619d9757bf80f82a8a24469d0f8a
JeniferArockiyasamy <115682637+JeniferArockiyasamy@users.noreply.github.com>
2022-10-25 10:38:06 +0530

    Add files via upload

---
+++ InsertionPoly.java
@@ -0,0 +1,38 @@
+package jeni;
+
+public class InsertionPoly {
+
+	public static void main(String[] args) {
+		InsertionPoly obj = new InsertionPoly();
+		int[] array1 = { 9, 5, 1, 4, 3 };
+		obj.sort(array1 );
+
+	}
+
+	private void sort(int[] array1) {
+		
+		System.out.println("Before Sorting");
+		for (int i = 0; i < array1.length; i++) {
+
+			System.out.print(array1[i] + " ");
+		}
+
+		for (int i = 1; i < array1.length; i++) {
+			int key = array1[i];
+			int j = i - 1;
+			while (j >= 0 && key < array1[j]) {
+				array1[j + 1] = array1[j];
+				--j;
+			}
+
+			array1[j + 1] = key;
+		}
+		System.out.println();
+		System.out.println("After Sorting");
+		for (int i = 0; i < array1.length; i++) {
+
+			System.out.print(array1[i] + " ");
+		}
+
+	}
+}

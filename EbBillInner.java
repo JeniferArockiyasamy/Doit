43b8c480e80ddcac83cc2181c551c39338694db8
JeniferArockiyasamy <115682637+JeniferArockiyasamy@users.noreply.github.com>
2022-10-25 10:21:30 +0530

    Add files via upload

---
+++ EbBillInner.java
@@ -0,0 +1,35 @@
+package jeni;
+
+import java.util.Scanner;
+
+public class EbBillInner {
+	class EbBillInner1 {
+		public void selection1() {
+
+			double bill = 0;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter the units here: ");
+			int unit = sc.nextInt();
+
+			if (unit < 100) {
+				bill = 100 * 0;
+				System.out.println("with in 100 units no need to pay");
+			} else if (unit <= 300) {
+				bill = (unit - 100) * 2;
+			} else if (unit < 400) {
+				bill = (unit - 100) * 3;
+			} else if (unit > 400) {
+				bill = (unit - 100) * 4;
+			}
+
+			System.out.println(bill);
+		}
+	}
+
+	public static void main(String[] args) {
+		EbBillInner in = new EbBillInner();
+		EbBillInner.EbBillInner1 inp = in.new EbBillInner1();
+		inp.selection1();
+	}
+
+}

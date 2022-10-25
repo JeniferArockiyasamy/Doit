47f4f2532a4b0d2d87961aae43a45eb09748b1dd
JeniferArockiyasamy <115682637+JeniferArockiyasamy@users.noreply.github.com>
2022-10-25 10:23:26 +0530

    Add files via upload

---
+++ EliminaterepOcc.java
@@ -0,0 +1,26 @@
+package jeni;
+
+import java.util.Scanner;
+
+public class EliminaterepOcc {
+
+	public static void main(String[] args) {
+		String str = "abccbdabbc";
+		String temp = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (temp.equals("")) {
+				temp += str.charAt(i);
+
+			} else {
+				if (temp.charAt(temp.length() - 1) == str.charAt(i)) {
+					temp = temp.substring(0, temp.length() - 1);
+				} else {
+					temp += str.charAt(i);
+
+				}
+			}
+		}
+		System.out.println("Result is: " + temp);
+	}
+
+}

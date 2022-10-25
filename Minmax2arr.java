5dff9efe9c7599ba543261a0cebec2da79ea308d
JeniferArockiyasamy <115682637+JeniferArockiyasamy@users.noreply.github.com>
2022-10-25 09:35:00 +0530

    Add files via upload

---
+++ Minmax2arr.java
@@ -0,0 +1,28 @@
+package jeni;
+
+public class Minmax2arr {
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int[][] arr1 = { { 98, 95, 92 }, { 101, 96, 99 } };
+			int max = arr1[0][0];
+			int min = arr1[0][0];
+			for (int i = 0; i < arr1.length; i++) {
+				for (int j = 0; j < arr1[i].length; j++) {
+					if (arr1[i][j] > max) {
+						max = arr1[i][j];
+					}
+
+					else if (arr1[i][j] < min) {
+						min = arr1[i][j];
+					}
+				}
+			}
+			System.out.println(max);
+			System.out.println(min);
+		}
+
+	}
+
+

520cfa912123ce60a923a727e8845a8e9ca7ef0f
JeniferArockiyasamy <115682637+JeniferArockiyasamy@users.noreply.github.com>
2022-10-25 10:40:42 +0530

    Add files via upload

---
+++ MapOccurance.java
@@ -0,0 +1,21 @@
+package jeni;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class MapOccurance {
+
+	public static void main(String[] args) {
+		String name = "goodmorning";
+		HashMap<Character, Integer> hm = new LinkedHashMap<Character, Integer>();
+		char[] ch = name.toCharArray();
+		for (char c : ch) {
+			if (hm.containsKey(c)) {
+				hm.put(c, hm.get(c) + 1);
+			} else {
+				hm.put(c, 1);
+			}
+		}
+		System.out.println(hm);
+	}
+}
